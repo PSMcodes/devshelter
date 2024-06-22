d3585c53de5c6f03ba5300db27a55db2f44e15d3
prasad <prasadkalvikatti@gmail.com>
2024-06-23 00:41:16 +0530

    minor bugs

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
         }, 1);
     };
     spinner();
-    
+
     var marquee_width = $(".marquee-content-primary").width();
     document.documentElement.style.setProperty('--marquee-padding', marquee_width + 'px');
 
@@ -106,7 +106,7 @@
             }
         }
     });
-    
+
     // $('.dropdown-submenu a.test').on("click", function(e){
     //     $(this).next('ul').toggle();
     //     e.stopPropagation();
@@ -117,33 +117,45 @@
     var myCarousel = document.querySelector('#myCarousel');
 
     var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 10000
-});
+        interval: 5000,
+        wrap:true
+    });
 
+    var myCarousel1 = document.querySelector('#myCarousel1');
 
-$('.carousel .carousel-item').each(function(){
-    var minPerSlide = 4;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<minPerSlide;i++) {
-        next=next.next();
+    var carousel = new bootstrap.Carousel(myCarousel1, {
+        interval: 3000,
+    });
+
+    var headerCarousel = document.querySelector('#header-carousel');
+    var carousel = new bootstrap.Carousel(headerCarousel, {
+        interval: 3000,
+        wrap: true
+    });
+
+    $('.carousel .carousel-item').each(function () {
+        var minPerSlide = 4;
+        var next = $(this).next();
         if (!next.length) {
-        	next = $(this).siblings(':first');
-      	}
-        
+            next = $(this).siblings(':first');
+        }
         next.children(':first-child').clone().appendTo($(this));
-      }
-});
+
+        for (var i = 0; i < minPerSlide; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
 
 
 
-  
-  
-    
+
+
+
 })(jQuery);
 
 // for contact form
